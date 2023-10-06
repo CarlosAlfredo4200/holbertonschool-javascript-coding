419a122e9708abdb9d347e2aa7672dfba612303f
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-10-06 12:05:42 -0500

    5-http.js

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,26 +1,20 @@
-const http = require("http");
-const countStudents = require("./3-read_file_async");
+const http = require('http');
+const countStudents = require('./3-read_file_async');
 
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-
-  if (req.url === "/") {
-    res.end("Hello Holberton School!\n");
-  } else if (req.url === "/students") {
-    res.write("This is the list of our students\n");
-
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') res.write('Hello Holberton School!');
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
     try {
       const data = await countStudents(process.argv[2]);
-      res.end(`${data.join("\n")}`);
+      res.end(`${data.join('\n')}`);
     } catch (error) {
       res.end(error.message);
     }
-  } else {
-    res.end("Not Found\n");
   }
+  res.end();
 });
-
 app.listen(1245);
-
 module.exports = app;
