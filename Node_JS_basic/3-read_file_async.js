78b4b01b9c9112be2133859d87ca655acdb84543
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-10-06 11:51:54 -0500

    3-read_file_async.js

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,36 +1,60 @@
-const fs = require('fs');
+const fs = require("fs");
+const { promisify } = require("util");
+
+// Promisify the fs.readFile method
+const readFileAsync = promisify(fs.readFile);
 
 function countStudents(path) {
-  const promise = (res, rej) => {
-    fs.readFile(path, 'utf8', (error, data) => {
-      if (error) rej(Error('Cannot load the database'));
-      const messages = [];
-      let message;
-      const content = data.toString().split('\n');
-      let students = content.filter((item) => item);
-      students = students.map((item) => item.split(','));
-      const nStudents = students.length ? students.length - 1 : 0;
-      message = `Number of students: ${nStudents}`;
-      console.log(message);
-      messages.push(message);
-      const subjects = {};
-      for (const i in students) {
-        if (i !== 0) {
-          if (!subjects[students[i][3]]) subjects[students[i][3]] = [];
-          subjects[students[i][3]].push(students[i][0]);
+  return new Promise((resolve, reject) => {
+    readFileAsync(path, "utf8")
+      .then((data) => {
+        // Split the file content into lines
+        const lines = data.split("\n").filter(Boolean); // Remove empty lines
+
+        // Initialize counts and lists for each field
+        let totalCount = 0;
+        const fieldCounts = {};
+        const fieldLists = {};
+
+        lines.forEach((line) => {
+          const [firstName, lastName, age, field] = line.split(",");
+
+          // Check if the line contains valid data
+          if (firstName && lastName && age && field) {
+            totalCount++;
+
+            // Update counts and lists for each field
+            if (!fieldCounts[field]) {
+              fieldCounts[field] = 1;
+              fieldLists[field] = [firstName];
+            } else {
+              fieldCounts[field]++;
+              fieldLists[field].push(firstName);
+            }
+          }
+        });
+
+        // Log the counts and lists
+        console.log(`Number of students: ${totalCount}`);
+        for (const field in fieldCounts) {
+          console.log(
+            `Number of students in ${field}: ${
+              fieldCounts[field]
+            }. List: ${fieldLists[field].join(", ")}`
+          );
         }
-      }
-      delete subjects.subject;
-      for (const key of Object.keys(subjects)) {
-        message = `Number of students in ${key}: ${
-          subjects[key].length
-        }. List: ${subjects[key].join(', ')}`;
-        console.log(message);
-        messages.push(message);
-      }
-      res(messages);
-    });
-  };
-  return new Promise(promise);
+
+        resolve(); // Resolve the Promise when done
+      })
+      .catch((err) => {
+        reject(new Error("Cannot load the database"));
+      });
+  });
 }
+
+// Example usage:
+countStudents("database.csv").catch((error) => {
+  console.error(error.message);
+});
+
 module.exports = countStudents;
---
+++ Node_JS_basic/prueba.js
@@ -0,0 +1,4 @@
+console.log("Escribe tu nombre:");
+const stdin = process.openStdin();
+
+console.log(stdin)
