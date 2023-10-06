d7a4a17fd824c51f3d2d184cd5635c8cc80975c7
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-10-06 11:53:06 -0500

    3-read_file_async.js

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,60 +1,36 @@
-const fs = require("fs");
-const { promisify } = require("util");
-
-// Promisify the fs.readFile method
-const readFileAsync = promisify(fs.readFile);
+const fs = require('fs');
 
 function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    readFileAsync(path, "utf8")
-      .then((data) => {
-        // Split the file content into lines
-        const lines = data.split("\n").filter(Boolean); // Remove empty lines
-
-        // Initialize counts and lists for each field
-        let totalCount = 0;
-        const fieldCounts = {};
-        const fieldLists = {};
-
-        lines.forEach((line) => {
-          const [firstName, lastName, age, field] = line.split(",");
-
-          // Check if the line contains valid data
-          if (firstName && lastName && age && field) {
-            totalCount++;
-
-            // Update counts and lists for each field
-            if (!fieldCounts[field]) {
-              fieldCounts[field] = 1;
-              fieldLists[field] = [firstName];
-            } else {
-              fieldCounts[field]++;
-              fieldLists[field].push(firstName);
-            }
-          }
-        });
-
-        // Log the counts and lists
-        console.log(`Number of students: ${totalCount}`);
-        for (const field in fieldCounts) {
-          console.log(
-            `Number of students in ${field}: ${
-              fieldCounts[field]
-            }. List: ${fieldLists[field].join(", ")}`
-          );
+  const promise = (res, rej) => {
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) rej(Error('Cannot load the database'));
+      const messages = [];
+      let message;
+      const content = data.toString().split('\n');
+      let students = content.filter((item) => item);
+      students = students.map((item) => item.split(','));
+      const nStudents = students.length ? students.length - 1 : 0;
+      message = `Number of students: ${nStudents}`;
+      console.log(message);
+      messages.push(message);
+      const subjects = {};
+      for (const i in students) {
+        if (i !== 0) {
+          if (!subjects[students[i][3]]) subjects[students[i][3]] = [];
+          subjects[students[i][3]].push(students[i][0]);
         }
-
-        resolve(); // Resolve the Promise when done
-      })
-      .catch((err) => {
-        reject(new Error("Cannot load the database"));
-      });
-  });
+      }
+      delete subjects.subject;
+      for (const key of Object.keys(subjects)) {
+        message = `Number of students in ${key}: ${
+          subjects[key].length
+        }. List: ${subjects[key].join(', ')}`;
+        console.log(message);
+        messages.push(message);
+      }
+      res(messages);
+    });
+  };
+  return new Promise(promise);
 }
-
-// Example usage:
-countStudents("database.csv").catch((error) => {
-  console.error(error.message);
-});
-
 module.exports = countStudents;
