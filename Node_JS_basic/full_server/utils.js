1201aac8a12ef74712fbef59c50bb987f7f4c7e1
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-10-06 15:36:59 -0500

    Update utils.js

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,31 +1,12 @@
-const fs = require('fs');
-const util = require('util');
-
-const readFileAsync = util.promisify(fs.readFile);
+const fs = require('fs').promises;
 
 async function readDatabase(filePath) {
   try {
-    const data = await readFileAsync(filePath, 'utf8');
-    const lines = data.split('\n').filter(Boolean);
-    const studentData = {};
-
-    lines.forEach((line) => {
-      const [firstName, , , field] = line.split(',');
-
-      if (firstName && field) {
-        if (!studentData[field]) {
-          studentData[field] = [];
-        }
-        studentData[field].push(firstName);
-      }
-    });
-
-    return studentData;
+    const data = await fs.readFile(filePath, 'utf8');
+    // Implement logic to parse the CSV and return the data as required.
   } catch (error) {
-    throw new Error('Cannot read the database');
+    throw new Error('Cannot load the database');
   }
 }
 
-module.exports = {
-  readDatabase,
-};
+module.exports = { readDatabase };
