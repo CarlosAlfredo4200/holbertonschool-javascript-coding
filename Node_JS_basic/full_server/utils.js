dfb8a51d83939d2dfae9e520f2d6e9f35cee910e
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-10-06 15:32:53 -0500

    Update utils.js

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,19 +1,31 @@
-const fs = require("fs");
+const fs = require('fs');
+const util = require('util');
 
-exports.readDatabase = (path) =>
-  new Promise((resolve, reject) => {
-    fs.readFile(path, { encoding: "utf-8" }, (err, data) => {
-      try {
-        if (err) reject(Error("Cannot load the database"));
-        const studentsInfo = data
-          .split("\n")
-          .filter((line) => line)
-          .slice(1);
-        return resolve({
-          studentsInfo,
-        });
-      } catch (err) {
-        return reject(Error("Cannot load the database"));
+const readFileAsync = util.promisify(fs.readFile);
+
+async function readDatabase(filePath) {
+  try {
+    const data = await readFileAsync(filePath, 'utf8');
+    const lines = data.split('\n').filter(Boolean);
+    const studentData = {};
+
+    lines.forEach((line) => {
+      const [firstName, , , field] = line.split(',');
+
+      if (firstName && field) {
+        if (!studentData[field]) {
+          studentData[field] = [];
+        }
+        studentData[field].push(firstName);
       }
     });
-  });
+
+    return studentData;
+  } catch (error) {
+    throw new Error('Cannot read the database');
+  }
+}
+
+module.exports = {
+  readDatabase,
+};
