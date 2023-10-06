b42879bd1ca1f06812c93318dba776a7edcf1465
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-10-06 15:28:04 -0500

    Update utils.js

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,39 +1,19 @@
-/*
-create a function named readDatabase that accepts a file path as argument:
+const fs = require("fs");
 
-  - It should read the database asynchronously
-  - It should return a promise
-  - When the file is not accessible, it should reject the promise with the error
-  - When the file can be read, it should return an object of arrays of the
-  firstname of students per fields
-*/
-
-const fs = require('fs');
-
-function readDatabase(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (error, students) => {
-      if (error) {
-        reject(Error('Cannot load the database'));
-        return;
+exports.readDatabase = (path) =>
+  new Promise((resolve, reject) => {
+    fs.readFile(path, { encoding: "utf-8" }, (err, data) => {
+      try {
+        if (err) reject(Error("Cannot load the database"));
+        const studentsInfo = data
+          .split("\n")
+          .filter((line) => line)
+          .slice(1);
+        return resolve({
+          studentsInfo,
+        });
+      } catch (err) {
+        return reject(Error("Cannot load the database"));
       }
-
-      let line = students.split('\n').slice(1); // remove the first line
-      line = line.filter((value) => value);
-
-      /* it should return an object of arrays of the firstname of students per fields */
-
-      const studentsByCS = line.filter((student) => student.includes('CS')).map((student) => student.split(',')[0]);
-      const studentsBySWE = line.filter((student) => student.includes('SWE')).map((student) => student.split(',')[0]);
-
-      const studentsByFields = {
-        CS: studentsByCS,
-        SWE: studentsBySWE,
-      };
-
-      resolve(studentsByFields);
     });
   });
-}
-
-module.exports = readDatabase;
