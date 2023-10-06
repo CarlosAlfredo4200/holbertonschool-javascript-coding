b42c09247f023d540eebb9c4d1b296f9a431eaed
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-10-06 14:28:30 -0500

    5-http.js

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,9 +4,9 @@ const countStudents = require("./3-read_file_async");
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  if (req.url === "/") res.write("Hello Holberton School!");
+  if (req.url === "/") res.end("Hello Holberton School!");
   if (req.url === "/students") {
-    res.write("This is the list of our students\n");
+    res.end("This is the list of our students\n");
     try {
       const data = await countStudents(process.argv[2]);
       res.end(`${data.join("\n")}`);
