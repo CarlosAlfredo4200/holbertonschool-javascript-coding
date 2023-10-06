43242bac256e510b459a736b4f0313cc04189556
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-10-06 15:24:46 -0500

    Update server.js

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,21 +1,11 @@
-/*
-create a small Express server:
-
-  - It should use the routes defined in full_server/routes/index.js
-  - It should use the port 1245
-
-*/
-
-const express = require('express');
-const path = require('path');
-const indexRouter = require('./routes/index');
+const express = require("express");
+const router = require("./routes/index");
 
 const app = express();
+const PORT = 12345;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use("/", router);
 
-app.use('/', indexRouter);
+app.listen(PORT);
 
-app.listen(1245);
+module.exports = app;
